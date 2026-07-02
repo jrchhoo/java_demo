07191ded58c683dd661d49d3ea96529a72d09689
Will <wmhou.xj@gmail.com>
2026-07-02 17:42:09 +0800

    Fix Spring Boot test package configuration

--- src/test/java/com/jrch/JrchApplicationTests.java
+++ src/test/java/com/jrch/JrchApplicationTests.java
@@ -1,13 +1,12 @@
-package com.wmh.utils;
+package com.jrch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UtilsApplicationTests {
+class JrchApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
